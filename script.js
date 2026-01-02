4846a104c6fe0ad72cbfc80f4fe47c0304494f95
msoomro <moomalmsoomro@gmail.com>
2026-01-01 19:13:37 -0500

    created working countdown timer and allow it to start, pause, and reset to a preset value with buttons

---
+++ script.js
@@ -0,0 +1,43 @@
+var minutes = 25;
+var seconds = 0;
+var x;
+function startCounter() {
+    // todo: troubleshoot why i'm losing the first second
+    document.getElementById('startbtn').disabled = true;
+    document.getElementById('pausebtn').disabled = false;
+    document.getElementById('resetbtn').disabled = false;
+
+    const targetDate = new Date().getTime() + (60000 * (document.getElementById('minutes').innerHTML)) + (1000 * (document.getElementById('seconds').innerHTML));
+
+    clearInterval(x);
+    x = setInterval(function() {
+        const distance = targetDate - new Date().getTime();
+
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("minutes").innerHTML = String(minutes).padStart(2, '0');
+        document.getElementById("seconds").innerHTML = String(seconds).padStart(2, '0');
+
+        if (distance <= 0) {
+            clearInterval(x);
+            document.getElementById('time').innerHTML="00:00";
+            alert("Your timer has ended!")
+        }
+    }, 250);
+}
+
+function pauseCounter() { //not working
+    document.getElementById('startbtn').disabled = false;
+    document.getElementById('pausebtn').disabled = true;
+    clearInterval(x);
+}
+
+function resetCounter() { //not working
+    document.getElementById('startbtn').disabled = false;
+    document.getElementById('pausebtn').disabled = true;
+    document.getElementById('resetbtn').disabled = true;
+    document.getElementById('minutes').innerHTML = "25";
+    document.getElementById('seconds').innerHTML = "00";
+    clearInterval(x);
+}
