b47504206665101442b28bec486c52f95d9b8c92
msoomro <moomalmsoomro@gmail.com>
2026-01-05 23:59:29 -0500

    Added hour functionality

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+var focusHours = 0;
 var focusMinutes = 25;
 var focusSeconds = 0;
+var break1Hours = 0;
 var break1Minutes = 15;
 var break1Seconds = 0;
+var break2Hours = 0;
 var break2Minutes = 5;
 var break2Seconds = 0;
 
@@ -12,20 +15,33 @@ function startCounter() {
     document.getElementById('pausebtn').disabled = false;
     document.getElementById('resetbtn').disabled = false;
 
-    const targetDate = new Date().getTime() + (60000 * (document.getElementById('minutes').innerHTML)) + (1000 * (document.getElementById('seconds').innerHTML));
+    const targetDate = new Date().getTime() + (60* 60000 * (document.getElementById('hours').innerHTML)) + (60000 * (document.getElementById('minutes').innerHTML)) + (1000 * (document.getElementById('seconds').innerHTML));
 
     clearInterval(x);
     x = setInterval(function() {
         const distance = targetDate - new Date().getTime();
 
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        if (hours > 0) {
+            document.getElementById("hours").innerHTML = String(hours).padStart(2, '0');
+            document.getElementById("hours").hidden = false;
+            document.getElementById("hourcolon").hidden = false;
 
+        }
+        else {
+            document.getElementById("hours").innerHTML = String(hours).padStart(2, '0');
+            document.getElementById("hours").hidden = true;
+            document.getElementById("hourcolon").hidden = true;
+        }
         document.getElementById("minutes").innerHTML = String(minutes).padStart(2, '0');
         document.getElementById("seconds").innerHTML = String(seconds).padStart(2, '0');
 
         if (distance <= 0) {
             clearInterval(x);
+            document.getElementById("hours").innerHTML = String(0).padStart(2,'0');
             document.getElementById("minutes").innerHTML = String(0).padStart(2,'0');
             document.getElementById("seconds").innerHTML = String(0).padStart(2,'0');
             alert("Your timer has ended!")
@@ -70,42 +86,46 @@ function toggleCountdownType(btn) {
 
 function updateTime() {
     var btn = document.getElementsByClassName("selectedTab")[0];
-    // alert(btn.id);
+    
     if (btn.id == "focus"){
+        document.getElementById("hours").innerHTML = String(focusHours).padStart(2,'0');
         document.getElementById("minutes").innerHTML = String(focusMinutes).padStart(2,'0');
         document.getElementById("seconds").innerHTML = String(focusSeconds).padStart(2,'0');
     }
     else if (btn.id == "break1"){
+        document.getElementById("hours").innerHTML = String(break1Hours).padStart(2,'0');
         document.getElementById("minutes").innerHTML = String(break1Minutes).padStart(2,'0');
         document.getElementById("seconds").innerHTML = String(break1Seconds).padStart(2,'0');
     }
     else if (btn.id == "break2"){
+        document.getElementById("hours").innerHTML = String(break2Hours).padStart(2,'0');
         document.getElementById("minutes").innerHTML = String(break2Minutes).padStart(2,'0');
         document.getElementById("seconds").innerHTML = String(break2Seconds).padStart(2,'0');
     }
 }
 
 function makeTimeEditable(element) {
-    // todo: only allow editing when timer is not counting down
-    // todo: save new time as reset
-    // todo: why does setting a 3-digit minute not work when hitting start?
-    // todo: only allow numbers to be inputted
+    // todo: only allow editing when timer is not counting down?
+    // todo: only allow numbers to be inputted --> need to change span to input... eh
     // todo: default to 00 if left empty? or do not change? for min do not change, for seconds default 0??
     element.contentEditable = true;
     element.focus();
-    element.onblur = function(){
-        // console.log(element.textContent);
+    element.onblur = function() {
+        
         var timerType = document.getElementsByClassName("selectedTab")[0].id;
-        // var inputType = element.id;
+        
         if (timerType=="focus") {
+            focusHours = document.getElementById("hours").innerHTML;
             focusMinutes = document.getElementById("minutes").innerHTML;
             focusSeconds = document.getElementById("seconds").innerHTML;
         }
         else if (timerType=="break1") {
+            break1Hours = document.getElementById("hours").innerHTML;
             break1Minutes = document.getElementById("minutes").innerHTML;
             break1Seconds = document.getElementById("seconds").innerHTML;
         }
         else if (timerType=="break2") {
+            break2Hours = document.getElementById("hours").innerHTML;
             break2Minutes = document.getElementById("minutes").innerHTML;
             break2Seconds = document.getElementById("seconds").innerHTML;
         }
